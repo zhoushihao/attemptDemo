966346444ea6dfa77abb8822c03d7445ad6e3379
zsh <enoch@enoch-car.com>
2017-07-20 09:25:16 +0800

    7.20

--- src/main/java/other/StringSubString.java
+++ src/main/java/other/StringSubString.java
@@ -6,9 +6,7 @@ package other;
 public class StringSubString {
 
     public static void main(String args[]){
-        String str = "E01PDD20170712064";
-        String sub = str.substring(str.length()-3);
-        Integer a = Integer.parseInt(sub);
+        Double a = 1.0/3;
         System.out.println(a);
     }
 
