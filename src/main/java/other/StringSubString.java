69067df8d750c42c6b85660635ee71bd51ff919a
zsh <enoch@enoch-car.com>
2017-07-25 10:28:02 +0800

    尝试

--- src/main/java/other/StringSubString.java
+++ src/main/java/other/StringSubString.java
@@ -1,13 +1,19 @@
 package other;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/7/13 0013.
  */
 public class StringSubString {
 
     public static void main(String args[]){
-        Double a = 1.0/3;
-        System.out.println(a);
+        List<Integer> ints = Arrays.asList(1,2,3,4,5,6);
+        for (int i=0;i<ints.size();i++){
+            int a = ints.get(i);
+            System.out.println(a);
+        }
     }
 
 }
