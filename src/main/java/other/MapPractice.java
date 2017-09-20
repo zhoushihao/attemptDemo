4d291dc5cac41083bdf58e68ae7e61ca786a6817
zsh <enoch@enoch-car.com>
2017-09-20 09:13:04 +0800

    init

--- src/main/java/other/MapPractice.java
+++ src/main/java/other/MapPractice.java
@@ -7,7 +7,9 @@ package other;
 public class MapPractice {
 
     public static void main(String[] strs) {
-
+       Double a = 1.1;
+       Double b = 1.11;
+       System.out.println(a.equals(b));
     }
 
 }
